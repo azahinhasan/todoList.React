cb7dac9f121bb8d01b2bfa728d5c11cfc03892f0
A H Zahin <azahinhasan@gmail.com>
2021-02-18 01:11:29 +0600

    css added

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -4,16 +4,26 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
+// var firebaseConfig = {
+//     apiKey: "AIzaSyA6Ok2eqATjs2yPi_MK6ET93Cpp1AoECoA",
+//     authDomain: "todolist-f5d0f.firebaseapp.com",
+//     databaseURL: "https://todolist-f5d0f-default-rtdb.firebaseio.com",
+//     projectId: "todolist-f5d0f",
+//     storageBucket: "todolist-f5d0f.appspot.com",
+//     messagingSenderId: "96480343942",
+//     appId: "1:96480343942:web:a585f34ef10a0455c89e07",
+//     measurementId: "G-1QQ94EK0EL"
+// };
+
 var firebaseConfig = {
-    apiKey: "AIzaSyA6Ok2eqATjs2yPi_MK6ET93Cpp1AoECoA",
-    authDomain: "todolist-f5d0f.firebaseapp.com",
-    databaseURL: "https://todolist-f5d0f-default-rtdb.firebaseio.com",
-    projectId: "todolist-f5d0f",
-    storageBucket: "todolist-f5d0f.appspot.com",
-    messagingSenderId: "96480343942",
-    appId: "1:96480343942:web:a585f34ef10a0455c89e07",
-    measurementId: "G-1QQ94EK0EL"
-};
+    apiKey: "AIzaSyAvqfm2zNHYHKAN75c_FwjViuJhe6HneaI",
+    authDomain: "todolist-4d2db.firebaseapp.com",
+    projectId: "todolist-4d2db",
+    storageBucket: "todolist-4d2db.appspot.com",
+    messagingSenderId: "611839853346",
+    appId: "1:611839853346:web:cfdf7461552546a6129b7b",
+    measurementId: "G-S32YXE5E9Z"
+  };
 
 
 firebase.initializeApp(firebaseConfig);
